d647e72178b10e97f377afe4cb27afa4d2ca4af5
ja-818 <julianarango1818@gmail.com>
2023-09-26 22:31:37 -0500

    v1

---
+++ script.js
@@ -0,0 +1,9 @@
+// window.onscroll = function() {
+//     const header = document.getElementById("main-header");
+//     if (window.scrollY > 50) {
+//         header.style.height = "50px";
+//     } else {
+//         header.style.height = "0";
+//     }
+
+// };
